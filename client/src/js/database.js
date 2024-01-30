3e4fcf56b38ae3a8f7142327430c96298f49bb3e
scnowak <143449653+scnowak@users.noreply.github.com>
2024-01-30 16:28:33 -0600

    working through TODOs in code

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,32 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// TODO: ADDED logic to a method that accepts some content and adds it to the database
+// export const putDb = async (content) => console.error('putDb not implemented');
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({id: 1, value: content});
+  const result = await request;
+  console.log("Updated text entry has been saved to the database!", result);
+  console.log("PUT to the database");
+};
+
+// TODO: ADDED logic for a method that gets all the content from the database
+// export const getDb = async () => console.error('getDb not implemented');
+
+export const getDb = async () => {
+  console.log("Text has been retrieved from the database.");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result.value);
+  console.log("GET all content from the database");
+  return result.value;
+};
 
 initdb();
